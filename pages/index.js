aa397553a00c94abae6c6a25084eec24f8a7b054
Javier Cervera <jvrmgmt@gmail.com>
2023-01-21 17:59:16 -0500

    third 2

--- pages/index.js
+++ pages/index.js
@@ -2,63 +2,68 @@ export default function Home() {
   return (
     <>
       <div className=" h-screen grid  content-center justify-items-center justify-center bg-stone-200">
-        <div className=" alexHeading text-3xl lg:text-5xl md:text-5xl  ">
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            A
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            l
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            e
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
-            j
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
-            a
-          </span>
-
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
-            n
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
-            d
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
-            r
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
-            o
-          </span>
-          <span> </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            M
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            a
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            l
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            d
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
-            o
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
-            n
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
-            a
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
-            d
-          </span>
-          <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
-            o
-          </span>
+        <div className=" alexHeading text-5xl lg:text-8xl md:text-6xl text-center  ">
+          <div>
+            {' '}
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              A
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              l
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              e
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+              j
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+              a
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+              n
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
+              d
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
+              r
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
+              o
+            </span>{' '}
+            <span> </span>
+          </div>
+          <div>
+            {' '}
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              M
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              a
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              l
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              d
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+              o
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+              n
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+              a
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
+              d
+            </span>
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-3s">
+              o
+            </span>
+          </div>
         </div>
         <div className="flex gap-10 mt-5">
           <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-1 px-5 border border-gray-400 rounded shadow">
