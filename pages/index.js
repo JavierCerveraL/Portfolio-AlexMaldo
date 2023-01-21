895245a4606b1e2bd4b9153a0847347969dda977
Javier Cervera <jvrmgmt@gmail.com>
2023-01-21 17:56:38 -0500

    third

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className=" h-screen grid  content-center justify-items-center justify-center bg-stone-200">
-        <div className=" alexHeading text-5xl  ">
+        <div className=" alexHeading text-3xl lg:text-5xl md:text-5xl  ">
           <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
             A
           </span>
@@ -61,13 +61,13 @@ export default function Home() {
           </span>
         </div>
         <div className="flex gap-10 mt-5">
-          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-1 px-5 border border-gray-400 rounded shadow">
             Portfolio
           </button>
-          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-1 px-5 border border-gray-400 rounded shadow">
             Contact me
           </button>
-          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-1 px-5 border border-gray-400 rounded shadow">
             Gallery
           </button>
         </div>
