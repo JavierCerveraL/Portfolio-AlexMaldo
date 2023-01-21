c9840e4f00474753566715c383b69aab2b3d615f
Javier Cervera <jvrmgmt@gmail.com>
2023-01-21 18:34:49 -0500

    smoth animation 4

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function Home() {
   return (
     <>
@@ -5,75 +7,84 @@ export default function Home() {
         <div className=" alexHeading text-7xl lg:text-8xl md:text-6xl text-center  ">
           <div>
             {' '}
-            <span className="animate__animated animate__slideInDown inline-block  ">
+            <span className="animate__animated animate__slideInDown inline-block   ">
               A
             </span>
-            <span className="animate__animated animate__slideInDown inline-block ">
+            <span className="animate__animated animate__slideInDown inline-block   ">
               l
             </span>
-            <span className="animate__animated animate__slideInDown inline-block ">
+            <span className="animate__animated animate__slideInDown inline-block  ">
               e
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s  ">
               j
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s  ">
               a
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s  ">
               n
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s  animate__slow">
               d
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s  animate__slow">
               r
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s  animate__slow">
               o
             </span>{' '}
             <span> </span>
           </div>
           <div>
             {' '}
-            <span className="animate__animated animate__slideInDown inline-block ">
+            <span className="animate__animated animate__slideInDown inline-block   ">
               M
             </span>
-            <span className="animate__animated animate__slideInDown inline-block ">
+            <span className="animate__animated animate__slideInDown inline-block   ">
               a
             </span>
-            <span className="animate__animated animate__slideInDown inline-block ">
+            <span className="animate__animated animate__slideInDown inline-block   ">
               l
             </span>
-            <span className="animate__animated animate__slideInDown inline-block ">
+            <span className="animate__animated animate__slideInDown inline-block   ">
               d
             </span>
-            <span className="animate__animated animate__slideInDown inline-block ">
+            <span className="animate__animated animate__slideInDown inline-block   ">
               o
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s  ">
               n
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-1s  ">
               a
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s  animate__slow">
               d
             </span>
-            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s">
+            <span className="animate__animated animate__slideInDown inline-block animate__delay-2s  animate__slow">
               o
             </span>
           </div>
         </div>
-        <div className="flex gap-2 mt-2">
-          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-1 px-5 border border-gray-400 rounded shadow">
+        <div className="flex gap-2 mt-2 animate__animated animate__bounce">
+          <button className="bg-none hover:bg-stone-100 text-stone-800 font-semibold py-1 px-5 border border-stone-400 rounded-sm shadow animate__animated animate__zoomIn  animate__delay-3s">
             Portfolio
           </button>
-          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-1 px-5 border border-gray-400 rounded shadow">
+          <button className="bg-none hover:bg-stone-100 text-stone-800 font-semibold py-1 px-5 border border-stone-400 rounded-sm shadow animate__animated animate__zoomIn animate__delay-3s">
             Contact me
           </button>
-          <button className="bg-none hover:bg-gray-100 text-stone-800 font-semibold py-1 px-5 border border-gray-400 rounded shadow">
+          <button className="bg-none hover:bg-stone-100 text-stone-800 font-semibold py-1 px-5 border border-stone-400 rounded-sm shadow animate__animated animate__zoomIn animate__delay-3s">
             Gallery
+          </button>
+        </div>
+
+        <div className="flex gap-2 mt-2 animate__animated animate__bounce">
+          <button className="bg-none hover:bg-stone-100 text-stone-800 font-semibold py-1 px-5 border border-stone-400 rounded-sm shadow animate__animated animate__zoomIn  animate__delay-3s">
+            About Me
+          </button>
+          <button className="bg-none hover:bg-stone-100 text-stone-800 font-semibold py-1 px-5 border border-stone-400 rounded-sm shadow animate__animated animate__zoomIn animate__delay-3s">
+            Services
           </button>
         </div>
       </div>
